a2a03b047fb3681092e21a07f8936034f67d3c98
Michele Memoli <michele.memoli@gmail.com>
2024-05-29 21:32:29 +0100

    feat(app): tailwind + shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  adapter: cloudflare(),
+  output: "server",
+  experimental: {
+    actions: true,
+  },
+});
